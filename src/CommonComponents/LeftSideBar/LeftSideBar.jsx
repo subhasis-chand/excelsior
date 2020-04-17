327278e7c2f028c1319e4c816b9605cda6dad748
subhasis-chand <subhasis.chand@gmail.com>
2020-04-17 23:42:30 +0530

    styles the sidebar

--- src/CommonComponents/LeftSideBar/LeftSideBar.jsx
+++ src/CommonComponents/LeftSideBar/LeftSideBar.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import './LeftSideBar.css';
+import { sidebarList } from './constants';
 
 class LeftSideBar extends Component {
 	constructor() {
-			super();
+		super();
+		this.state = {
+			selectedList: '/',
 		}
+	}
+
+	handleClick = (selectedList) => {
+		this.setState({selectedList});
+	}
 
 	render() {
+		const selectedList = this.state.selectedList;
+		const finalList = sidebarList.map((element, index) => {
+			return(
+				<Link to={'/' + element.route} key={index}>
+					<li onClick={() => this.handleClick(element.route)}
+						className={ selectedList === element.route ? 'list-selected' : 'list-unselected' }>
+						{ element.displayName }
+					</li>
+				</Link>
+			)
+		})
 		return(
 			<div className='left-side-bar-size left-side-bar'>
-				<Link to='/'><li className='list'>HOME</li></Link>
-				<Link to='/process_file'><li className='list'>Process File</li></Link>
-				<Link to='/pca'><li className='list'>PCA</li></Link>
-				<Link to='/linear_regression'><li className='list'>Linear Regression</li></Link>
-				<Link to='/logistic_regression'><li className='list'>Logistic Regression</li></Link>
-				<Link to='/neural_networks'><li className='list'>Neural Networks</li></Link>
-				<Link to='/decision_tree'><li className='list'>Decision Trees</li></Link>
+				{finalList}
 			</div>
 		)
 	}
---
+++ src/CommonComponents/LeftSideBar/constants.jsx
@@ -0,0 +1,10 @@
+export const sidebarList =
+[
+	{route: '', displayName: 'Home'},
+	{route: 'process_file', displayName: 'Process File'},
+	{route: 'pca', displayName: 'PCA'},
+	{route: 'linear_regression', displayName: 'Linear Regression'},
+	{route: 'logistic_regression', displayName: 'Logistic Regression'},
+	{route: 'neural_networks', displayName: 'Neural Networks'},
+	{route: 'decision_tree', displayName: 'Decision Tree'},
+]
