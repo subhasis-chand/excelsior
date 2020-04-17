2c628217269fef9a513a9f606a9d343b0a2a363f
subhasis-chand <subhasis.chand@gmail.com>
2020-04-17 13:37:19 +0530

    creates initial routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Home from './Pages/Home';
 import ProcessFile from './Pages/ProcessFile';
-import LinearRegression from './Pages/LinearRegression'
+import LinearRegression from './Pages/LinearRegression';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home/>
-      <ProcessFile/>
-      <LinearRegression/>
-    </div>
+    <Router>
+      <div>
+        {/* <Header/> */}
+        <Switch>
+          <Route path='/' exact component={ Home } />
+          <Route path='/process_file' exact component={ ProcessFile } />
+          <Route path='/linear_regression' exact component={ LinearRegression } />
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
