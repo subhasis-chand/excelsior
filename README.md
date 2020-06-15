4a9a7e21418e82ef212d3dfb2e00a65893917fe7
subhasis-chand <subhasis.chand@gmail.com>
2020-06-15 16:30:09 +0530

    added home and updated read me

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Excelsior
-The GUI for machine learning. Do ML without coding...
+The GUI for machine learning.
+
+Excelsior is a powerful one of a kind web application which allows the user to apply 
+concepts of machine learning without a single line of coding. User can apply various machine 
+learning algorithms on their own data using a well designed GUI.
+
+Processing files and dimentionality reduction was never this easy before. With a few clicks
+user can do the same. The processed file is stored in the back end automatically.
+It can definitely be downloaded. But if the same file has to be used again, there is 
+no need to re-upload.
+
+Cassical machine learning algorithms such as Linear Regression, Logistic Regression and 
+Decision Trees can be implemented with just a few clicks. The confusion matrix
+and other results and graphs are plotted automatically.
+
+The most important feature is the Neural Networks. Even deep nets can be trained using 
+excelsior. There are no application which let you add layers in neural net with click of buttons.
+Excelsior provides the best way to build neural nets and test your data.
+
 
 
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,7 +14,31 @@ export default class Home extends Component {
 		return(
 			<div className='app-body'>
 				<LeftSideBar path={ path }/>
-				<div className='dark-background main-content-size'>Home</div>
+				<div className='dark-background main-content-size'>
+					<div style={{
+						fontSize: '18px',
+						margin: '10px',
+						padding: '10px',
+						color: 'coral',
+						border: '1px solid grey',
+						textAlign: 'justify'
+					}}>
+						<p>Excelsior is a powerful one of a kind web application which allows the user to apply 
+							concepts of machine learning without a single line of coding. User can apply various machine 
+							learning algorithms on their own data using a well designed GUI. <br/><br/>
+							Processing files and dimentionality reduction was never this easy before. With a few clicks
+							user can do the same. The processed file is stored in the back end automatically.
+							It can definitely be downloaded. But if the same file has to be used again, there is 
+							no need to re-upload<br/> <br/>
+							Cassical machine learning algorithms such as Linear Regression, Logistic Regression and 
+							Decision Trees can be implemented with just a few clicks. The confusion matrix
+							and other results and graphs are plotted automatically.<br/><br/>
+							The most important feature is the Neural Networks. Even deep nets can be trained using 
+							excelsior. There are no application which let you add layers in neural net with click of buttons.
+							Excelsior provides the best way to build neural nets and test your data.
+						</p>
+					</div>
+				</div>
 			</div>
 		)
 	}
